3ced89e4f4ab380ff1b6eef9c11da851c755efd1
ntthai2 <thaint753@gmail.com>
2026-03-22 19:00:50 +0700

    chore: remove unused models

--- APGCC/apgcc/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .build import loading_data
-
-def build_dataset(cfg):
-    return loading_data(cfg)
--- APGCC/apgcc/test.sh
+++
@@ -1,1 +0,0 @@
-python main.py -t -c ./configs/SHHA_test.yml TEST.WEIGHT './output/SHHA_best.pth' OUTPUT_DIR ./output/ TEST.THRESHOLD 0.5
